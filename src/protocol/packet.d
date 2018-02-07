c9a16f1e2132d7d9e8e8f7857dcd926df61020c3
Miguel Pelaez <kernelfreeze@outlook.com>
2018-02-07 12:58:59 -0500

    Serializing data

--- src/protocol/packet.d
+++ src/protocol/packet.d
@@ -1,5 +1,5 @@
 import vibe.d;
-import std.bitmanip : peek;
+import std.bitmanip : peek, nativeToBigEndian;
 
 /// Input stream operations
 public abstract class Readable {
@@ -13,9 +13,19 @@ public abstract class Readable {
         return o;
     }
 
-    /// Read an incoming signed byte
+    /// Read incoming signed bytes
     protected byte[] readBytes(int amount = 1) {
         return cast(byte[]) readUBytes(amount);
+    }
+
+    /// Read an incoming byte
+    protected byte readUByte() {
+        return readUBytes[0];
+    }
+
+    /// Read an incoming signed byte
+    protected byte readByte() {
+        return readBytes[0];
     }
 
     /// Read an incoming boolean
@@ -48,7 +58,7 @@ public abstract class Readable {
         return readUBytes(8).peek!long;
     }
 
-     /// Read an incoming single-precision 32-bit floating point number
+    /// Read an incoming single-precision 32-bit floating point number
     protected float readFloat() {
         return readUBytes(4).peek!float;
     }
@@ -57,11 +67,102 @@ public abstract class Readable {
     protected double readDouble() {
         return readUBytes(8).peek!double;
     }
+
+    /// Read an incoming variable size integer
+    protected int readVarInt() {
+        int numRead;
+        int result;
+        byte read;
+
+        do {
+            read = readByte;
+            const int value = (read & 0b01111111);
+            result |= (value << (7 * numRead));
+
+            numRead++;
+            if (numRead > 5) {
+                throw new Exception("VarInt is too big");
+            }
+        }
+        while ((read & 0b10000000) != 0);
+
+        return result;
+    }
+
+    /// Read an incoming variable size integer
+    protected long readVarLong() {
+        int numRead;
+        long result;
+        byte read;
+
+        do {
+            read = readByte;
+            const int value = (read & 0b01111111);
+            result |= (value << (7 * numRead));
+
+            numRead++;
+            if (numRead > 10) {
+                throw new Exception("VarLong is too big");
+            }
+        }
+        while ((read & 0b10000000) != 0);
+
+        return result;
+    }
 }
 
 /// IO stream operations
 public abstract class Writeable : Readable {
-    
+    protected void writeByte(byte b) {
+        ubyte[1] o = [cast(ubyte) b];
+        connection.write(o);
+    }
+
+    protected void writeUByte(ubyte b) {
+        ubyte[1] o = [b];
+        connection.write(o);
+    }
+
+    protected void writeBool(bool input) {
+        connection.write(nativeToBigEndian(input));
+    }
+
+    protected void writeShort(short input) {
+        connection.write(nativeToBigEndian(input));
+    }
+
+    protected void writeUShort(ushort input) {
+        connection.write(nativeToBigEndian(input));
+    }
+
+    protected void writeInt(int input) {
+        connection.write(nativeToBigEndian(input));
+    }
+
+    protected void writeLong(long input) {
+        connection.write(nativeToBigEndian(input));
+    }
+
+    protected void writeFloat(float input) {
+        connection.write(nativeToBigEndian(input));
+    }
+
+    protected void writeDouble(double input) {
+        connection.write(nativeToBigEndian(input));
+    }
+
+    protected void writeVarInt(int value) {
+        do {
+            byte temp = cast(byte) (value & 0b01111111);
+
+            value >>>= 7;
+            if (value != 0) {
+                temp |= 0b10000000;
+            }
+            writeByte(temp);
+        }
+        while (value != 0);
+    }
 }
 
 /// Packet base
